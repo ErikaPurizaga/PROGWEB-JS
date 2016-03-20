7d6d3627b420e6bf49023446a012d709209ef16e
Erika-Liliana <eri.purizaga92@gmail.com>
2016-03-20 15:40:48 -0500

    Errores corregidos 2-taller

--- scripts.js
+++ scripts.js
@@ -9,6 +9,6 @@ alert("Atención Nueva página en desarrollo");
 document.write("<h1>Hola Compañeras Colaboradoras de Desarrollo</h1>");
 document.write("<h1>Colaboren en la corrección de unos errores</h1>");
 document.write("<h1>No los podemos resolver</h1>");/*error corregido */
-documentwrite("<h1>Su colaboracion es importante</h1>");
-alert("Gracias por la Revisión y Corrección);
-console.log("Fin de taller JS")
+document.write("<h1>Su colaboracion es importante</h1>");/*error corregido2*/
+alert("Gracias por la Revisión y Corrección");/*2error corregido 2*/
+console.log("Fin de taller JS"); /*3 error  corregido2*/
