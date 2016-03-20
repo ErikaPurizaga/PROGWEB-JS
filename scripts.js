80703008e1a420b8ceb71fcd67ae25e226f5cfd1
Erika-Liliana <eri.purizaga92@gmail.com>
2016-03-20 17:17:55 -0500

    Desafio-Cuenta cuentos

--- scripts.js
+++ scripts.js
@@ -5,8 +5,26 @@ alert("Gracias por visitar");
 console.log("Fin de programa");*/ /*error corregido  de mas*/
 
 
-console.log("Inicio de programa");
+/*VARIABLES*/
+/*console.log("Inicio de programa");
 var nombre = prompt("Cual es tu nombre?");
 document.write ("<h1> Su nombre es " +nombre);
 console.log("Su nombre es:" +nombre);
-console.log("Fin de Programa");
+console.log("Fin de Programa");*/
+
+
+/*variables ejercicios
+var clave =prompt("Hola Usuario,Ingrese su clave en minusculas!");
+console.log("La longitud de la clave:" + clave.length);
+console.log("TA-DA!!! Su clave es:"+ clave.toUpperCase());
+console.log("El valor de la clave es:" + clave);*/
+
+/*Desafio Cuenta Cuentos - Concatenacion*/
+var sustantivo =prompt("Hola ,Ingresa un sustantivo!");
+var adjetivo =prompt("Hola ,Ingresa un adjetivo!");
+var verbo =prompt("Hola ,Ingresa un verbo en infinitivo!");
+document.write("<h1>El "+ adjetivo + " "+ sustantivo + " queria " + verbo + " con sus amigos en el patio de su caleta");
+document.write("<h1>El "+ sustantivo + " "+ adjetivo + " queria " + verbo + " antes de pegarse un bañito");
+document.write("<h1>El "+ adjetivo + " "+ sustantivo + " queria " + verbo + " solo porque quería ser popular");
+
+
