21228778715a3a914495e1de90b7d284ee3c1aa8
Erika-Liliana <eri.purizaga92@gmail.com>
2016-03-20 16:34:14 -0500

    Nuevo script - uso de variables

--- scripts.js
+++ scripts.js
@@ -2,13 +2,11 @@
 alert("Hola JavaScript");																																																							
 document.write("<h1>Hola JavaScript!</h1>");
 alert("Gracias por visitar");
-console.log("Fin de programa");*/ /**error corregido / de mas*/
+console.log("Fin de programa");*/ /*error corregido  de mas*/
 
-console.log("Inicio de taller JS"); /*error corregido*/
-alert("Atención Nueva página en desarrollo");																																																							
-document.write("<h1>Hola Compañeras Colaboradoras de Desarrollo</h1>");
-document.write("<h1>Colaboren en la corrección de unos errores</h1>");
-document.write("<h1>No los podemos resolver</h1>");/*error corregido */
-document.write("<h1>Su colaboracion es importante</h1>");/*error corregido2*/
-alert("Gracias por la Revisión y Corrección");/*2error corregido 2*/
-console.log("Fin de taller JS"); /*3 error  corregido2*/
+
+console.log("Inicio de programa");
+var nombre = prompt("Cual es tu nombre?");
+document.write ("<h1> Su nombre es " +nombre);
+console.log("Su nombre es:" +nombre);
+console.log("Fin de Programa");
