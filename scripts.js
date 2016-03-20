a2f063fb635fc4add2d901f2907a5a884ba2f2cb
RITA Y ROXANA <ritafabiola3010@hotmail.com>
2016-03-20 15:24:18 -0500

    corregido errores

--- scripts.js
+++ scripts.js
@@ -2,13 +2,13 @@
 alert("Hola JavaScript");																																																							
 document.write("<h1>Hola JavaScript!</h1>");
 alert("Gracias por visitar");
-console.log("Fin de programa");*//
+console.log("Fin de programa");*/ /**error corregido / de mas/
 
-console.log ("Inicio de taller JS");
+console.log("Inicio de taller JS"); /*error corregido*/
 alert("Atención Nueva página en desarrollo");																																																							
 document.write("<h1>Hola Compañeras Colaboradoras de Desarrollo</h1>");
 document.write("<h1>Colaboren en la corrección de unos errores</h1>");
-Document.write("<h1>No los podemos resolver</h1>");
+document.write("<h1>No los podemos resolver</h1>");/*error corregido */
 document.write("<h1>Su colaboracion es importante</h1>");
 alert("Gracias por la Revisión y Corrección");
 console.log("Fin de taller JS");
