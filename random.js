373df709d819c720b8d22bb0cccd714ca9b13888
Erika-Liliana <eri.purizaga92@gmail.com>
2016-03-27 12:24:24 -0500

    Funciones tiraDados - random.js

---
+++ random.js
@@ -0,0 +1,9 @@
+console.log ("Inicio del Programa");
+function tiraDados (){
+	var dado = Math.floor(Math.random()*6)+1;
+	alert(dado);
+}
+alert("Tiramos 3 dados ");
+tiraDados();
+tiraDados();
+tiraDados();
